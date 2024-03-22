e5a9c834ff14fc6d2be023debf14b2756a8caa4b
Alireza Rezaei <rezaey@gmail.com>
2024-03-22 13:31:48 +0330

    creating event model and registering it to admin.site

--- event_scheduler/app/admin.py
+++ event_scheduler/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from  .models import Event
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display=('title','Start At','duration')
+admin.site.register(Event,EventAdmin)
--- event_scheduler/app/models.py
+++ event_scheduler/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Event(models.Model):
+    title=models.CharField(max_length=100)
+    body=models.TextField(max_length=2024)
+    start=models.DateTimeField(name='Start At')
+    duration=models.DurationField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+

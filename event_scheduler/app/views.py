8ae831ade1d07206465c6b1f9945857785cf46bb
Alireza Rezaei <rezaey@gmail.com>
2024-03-25 01:26:43 +0330

    Adding simple css file to mark events based on time.

--- event_scheduler/app/views.py
+++ event_scheduler/app/views.py
@@ -3,15 +3,23 @@ from .models import Event
 from django.template import loader
 from django.http import HttpResponse
 # Create your views here.
-from datetime import datetime
+from datetime import datetime,timezone
 
 def events(request):
     template=loader.get_template('events.html')
     events=Event.objects.all()
-    now=datetime.now()
+    now=datetime.now(timezone.utc)
     for event in events :
-        if event.start <now:
-            pass
+        start=event.start
+        duration = event.duration
+        if start+duration <now:
+            status='past'
+        elif start < now :
+            status='present'
+        else:
+            status='future'
+
+        event.timestatus=status    
     context={
         'events': events
     }
